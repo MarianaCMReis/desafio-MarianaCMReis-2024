43dd21051306fd21f0246722e16d77a21dfddbc0
mariana <marianachristine31@gmail.com>
2024-09-12 22:18:37 -0300

    adicionando arquivo de recintos existentes

---
+++ src/recintos-existentes.js
@@ -0,0 +1,10 @@
+//RECINTOS EXISTENTES - O zoológico possui os seguintes recintos disponíveis.
+const recintos = [
+    { numero: 1, biomas: ["savana"], tamanhoTotal: 10, animais: [{ nome: "MACACO", quantidade: 3 }] },
+    { numero: 2, biomas: ["floresta"], tamanhoTotal: 5, animais: [] },
+    { numero: 3, biomas: ["savana", "rio"], tamanhoTotal: 7, animais: [{ nome: "GAZELA", quantidade: 1 }] },
+    { numero: 4, biomas: ["rio"], tamanhoTotal: 8, animais: [] },
+    { numero: 5, biomas: ["savana"], tamanhoTotal: 9, animais: [{ nome: "LEAO", quantidade: 1 }] }
+];
+
+export { recintos };
